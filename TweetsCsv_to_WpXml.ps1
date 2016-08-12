9379a04dba9d54b259acb36d4051700afe037d7d
Akiya <autumnsky64@gmail.com>
2016-08-12 22:16:18 +0900

    アウトラインを記述

--- TweetsCsv_to_WpXml.ps1
+++ TweetsCsv_to_WpXml.ps1
@@ -1,1 +1,26 @@
-﻿
+<#
+Args 
+    Required ---currently hard coding
+    StartDay
+    EndDay
+    Twitter ScreenName
+
+    optional
+    Month / Day Switch
+    Author
+    Category slug
+
+Post meta 
+    1. publish date ... tweet +1 day AM 00:00
+    2. author ... empty is allowed
+    3. post-entry ... ul> li> "tweet text"
+    4. category ... tweet
+#>
+
+#1. load xml template
+
+#2. get post entry = ul list include tweets.
+
+#3. build single entry node
+
+#4. add node to xml template
