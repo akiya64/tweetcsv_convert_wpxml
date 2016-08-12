f641f5099be60aae20700df80c2debff630aa243
Akiya <autumnsky64@gmail.com>
2016-08-12 22:16:59 +0900

    コメントにデータ構造を記述した。

--- test_build_list.ps1
+++ test_build_list.ps1
@@ -1,5 +1,5 @@
 ﻿<#
-test
+trial create ul list from SQL result 
 #>
 
 $StartDay =  Get-Date(Get-Date -Date 2009-07-03)
@@ -10,14 +10,23 @@ $Day = $StartDay.AddDays(7).ToString("yyyy-MM-dd")
 echo $Day
     
 $Title = "Twitter Updates for " + $Day
-    
+
 $ResultQ = q -d ',' "`"SELECT c1,c6 FROM tweets.csv WHERE c4 LIKE `'$day%`' AND c6 NOT Like `'@%`'`""
+
+<# Data set
+                    id , text
+ResultQ[0] = 1234567890, tweet 
+ResultQ[1]   1234678099, tweet2
+    :
+#>
 
 foreach($Row in $ResultQ){
     
     $TwArr = $Row.Split(",")
 
     $TwText = $TwArr[1]
+
+    # make permanent URI
     $TwUrL = "https://twitter.com/K_akiya/status/" + $TwArr[0]
 
 $Tw = @"
