50060c27d8cf9a756caf9a0d239a78bdd05c0cc3
Akiya <autumnsky64@gmail.com>
2016-08-13 18:17:15 +0900

    モジュールとした。

--- tweets_slice.ps1
+++ tweets_slice.ps1
@@ -10,31 +10,13 @@ Tweet text by Unique ID
 q -E sjis -d ',' "SELECT c6 FROM tweets WHERE c1='763551255906004992'"
 #>
 
+Param([String]$Day)
+
 Set-Location (Split-Path $MyInvocation.MyCommand.Path -parent)
 
-echo "Get Records Count ..."
+# Select by Column No.Cutoff header. And timstamp is string, so use "like" syntax
+$Query = "`"SELECT c1,c6 FROM ./tweets.csv WHERE c4 LIKE `'$Day%`' AND c6 NOT Like `'@%`'`""
 
-$c = q -d ',' "SELECT COUNT(*) FROM tweets.csv"
+$ResultQ = q -d ',' $Query
 
-echo "$c Tweets."`r`r
-
-$StartDay =  Get-Date(Get-Date -Date 2009-07-03)
-$EndDay = Get-Date(Get-Date -Date 2011-07-03)
-
-for ($i = 10 ;  $i -lt 15 ; $i++){ 
-    $Day = $StartDay.AddDays($i).ToString("yyyy-MM-dd")
-
-    $Title = "Twitter Updates for " + $Day
-    
-    echo $Title
-
-    # Select by Column No.Cutoff header. And timstamp is string, so use "like" syntax
-    $Query = "`"SELECT c1,c6 FROM ./tweets.csv WHERE c4 LIKE `'$day%`' AND c6 NOT Like `'@%`'`""
-
-    $DaysTweet = q -d ',' $Query
-
-    echo $DaysTweet
-    echo `r
-
-    Get-TypeData($DaysTweet)
-        }
+return $ResultQ
